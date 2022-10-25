448a3daac0570c5752734ee959cc6181b024f405
Khlff <n.hlopunov@ya.ru>
2022-10-26 00:27:39 +0500

    upd

--- src/main/java/BotLogic.java
+++ src/main/java/BotLogic.java
@@ -1,25 +1,13 @@
 import java.util.Objects;
 
 public class BotLogic {
-    public String answer(String message) {
-        switch (parseUserMessage(message)) {
-            case HELP:
-                return StandardAnswer.answerForHelp;
-            case START:
-                return StandardAnswer.answerForStart;
-            case JOKE:
-                return StandardAnswer.answerForJoke;
-            case TEACHER:
-                return """
-                        tg: @a_lgnv
-                        Расписание:
-                            Среда: 10:40 - 12:10 каб.517""";
-        }
-        return StandardAnswer.defaultAnswer;
+    public String get_answer(String message) {
+        return switch (parseUserMessage(message)) {
+            case START -> DefaultAnswers.StartAnswer;
+            case HELP -> DefaultAnswers.HelpAnswer;
+            default -> DefaultAnswers.defaultAnswer;
+        };
     }
-
-
-//  todo*: не хардкодить здесь команды, генироровать хелп только по тому, что точно умеем
 
 
     private UserRequest.UserRequests parseUserMessage(String message) {
